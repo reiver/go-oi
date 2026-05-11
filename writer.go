2c810738da0492908d7424345256dcd024b7d6f6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-05-11 14:14:33 -0700

    oi.Writer

---
+++ writer.go
@@ -0,0 +1,6 @@
+package oi
+
+// Writer represents a modernized version of [io.Writer], where the Write method has a [context.Context] as its first parameter.
+type Writer interface {
+	Write(ctx context.Context, p []byte) (n int, err error)
+}
